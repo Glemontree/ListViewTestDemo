e6f0a7faa3ba117d8676d9722f2dd19507049df0
gtp <gtp0521@gmail.com>
2016-10-15 14:12:25 +0800

    first commit

---
+++ app/src/main/java/com/glemontree/listviewtest/Fruit.java
@@ -0,0 +1,20 @@
+package com.glemontree.listviewtest;
+
+/**
+ * Created by Administrator on 2016/10/15.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+}
